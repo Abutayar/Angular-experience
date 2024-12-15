cca4a2e8b50f76fc7505b93b8bc60867a2e3bdf3
Abutayar Shaikh <abutayarshaikh@hotmail.com>
2024-12-15 15:35:27 +0530

    added home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ExampleInputStateBasedOnCheckboxComponent } from './pages/example-input-state-based-on-checkbox/example-input-state-based-on-checkbox.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'example-custom-form-state-directive',
+        component: ExampleInputStateBasedOnCheckboxComponent
+    }
+];
